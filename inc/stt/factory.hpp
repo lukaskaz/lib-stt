09f10e2d2b5ef83b349ba8529f0ed6bff86f7259
Łukasz Kaźmierczak <lukask84@o2.pl>
2024-10-25 21:48:52 +0200

    Usage of factory updated
    Signed-off-by: Łukasz Kaźmierczak <lukask84@o2.pl>

--- inc/stt/factory.hpp
+++ inc/stt/factory.hpp
@@ -9,23 +9,25 @@
 namespace stt
 {
 
-template <typename T>
 class TextFromVoiceFactory
 {
   public:
+    template <typename T>
     static std::shared_ptr<TextFromVoiceIf> create(language lang)
     {
         auto shell = std::make_shared<shell::BashCommand>();
-        return create(shell, lang);
+        return create<T>(shell, lang);
     }
 
+    template <typename T>
     static std::shared_ptr<TextFromVoiceIf>
         create(std::shared_ptr<shell::ShellCommand> shell, language lang)
     {
         auto helpers = stthelpers::HelpersFactory::create();
-        return create(shell, helpers, lang);
+        return create<T>(shell, helpers, lang);
     }
 
+    template <typename T>
     static std::shared_ptr<TextFromVoiceIf>
         create(std::shared_ptr<shell::ShellCommand> shell,
                std::shared_ptr<stthelpers::HelpersIf> helpers, language lang)
